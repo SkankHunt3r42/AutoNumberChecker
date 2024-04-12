6e5ee25ba6235d43a5cff5704c2853a055af89c7
SkankHunt3r42 <nikitos77@i.ua>
2024-04-12 21:16:36 +0300

    Add a core-program

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+
+    static  Scanner scanner = new Scanner(System.in);
+    static  String staticPattern = "^\\+380\\d{9}$";
+
+    static int count;
+
+    static int countNumbersArry() {
+        System.out.println("Enter numbers of array");
+        count = scanner.nextInt();
+        scanner.skip("\\R");
+        return count ;
+    }
+
+
+    public static void main(String[] args) {
+        String[] MobileNums = new String[countNumbersArry()];
+        for (int i = 0; i < MobileNums.length; i++) {
+            MobileNums[i] = scanner.nextLine();
+        }
+        for (String numbers : MobileNums) {
+            boolean valid = validateMobileNumber(numbers);
+            System.out.println(numbers + " - " + (valid ? " number which added to array MobileNums are valid" : " " +
+                    "Current numbers is invaild,please try again"));
+        }
+    }
+    private static boolean validateMobileNumber(String number) {
+        Pattern compiledPatter = Pattern.compile(staticPattern);
+        Matcher matcher = compiledPatter.matcher(number);
+
+        return  matcher.matches();
+    }
+}
+
+
+
+
+
