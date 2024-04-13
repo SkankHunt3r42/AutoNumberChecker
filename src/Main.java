ad9fa17b8ecbecdae188c1ddd6466c1278fac262
SkankHunt3r42 <nikitos77@i.ua>
2024-04-13 18:50:11 +0300

    fix some troubles

--- src/Main.java
+++ src/Main.java
@@ -6,20 +6,17 @@ public class Main {
 
     static  Scanner scanner = new Scanner(System.in);
     static  String staticPattern = "^\\+380\\d{9}$";
-
     static int count;
-
     static int countNumbersArry() {
-        System.out.println("Enter numbers of array");
+        System.out.print("Enter count of numbers that you want to check: ");
         count = scanner.nextInt();
         scanner.skip("\\R");
         return count ;
     }
-
-
     public static void main(String[] args) {
         String[] MobileNums = new String[countNumbersArry()];
         for (int i = 0; i < MobileNums.length; i++) {
+            System.out.printf("Enter number " + (i + 1) + ": " );
             MobileNums[i] = scanner.nextLine();
         }
         for (String numbers : MobileNums) {
@@ -31,11 +28,9 @@ public class Main {
     private static boolean validateMobileNumber(String number) {
         Pattern compiledPatter = Pattern.compile(staticPattern);
         Matcher matcher = compiledPatter.matcher(number);
-
         return  matcher.matches();
     }
 }
-
 
 
 
